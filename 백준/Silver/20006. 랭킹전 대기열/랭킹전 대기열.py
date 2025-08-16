b5fb8d793822eca4aaf25ff57029e0f77e45fc48
이상민 <sm1118sm@gmail.com>
2025-08-16 14:26:51 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -0,0 +1,28 @@
+p, m = map(int, input().split())
+
+rooms = []  
+
+for _ in range(p):
+    level, name = input().split()
+    if len(rooms) == 0:     
+        rooms.append([[int(level), name]])    
+        
+    else:    
+        for room in rooms:      
+            first_level = room[0][0]     
+            if len(room) < m and first_level - 10 <= int(level) <= first_level + 10:
+                room.append([level, name])  
+                break     
+                
+        else:     
+            rooms.append([[int(level), name]])     
+    
+for room in rooms:
+    if len(room) == m:
+        print('Started!')
+    else:
+        print('Waiting!')
+    room.sort(key=lambda x:x[1]) 
+    
+    for player in room:
+        print(player[0], player[1])
