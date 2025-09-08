3a8b5fe226194ac5118c198c06bfce6c679046ab
이상민 <sm1118sm@gmail.com>
2025-09-08 18:58:24 +0900

    [Silver I] Title: 점프, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0] * n for _ in range(n)]
+dp[0][0] = 1
+
+for i in range(n):
+    for j in range(n):
+        if dp[i][j] == 0:
+            continue
+
+        jump = graph[i][j]
+
+        if jump == 0:
+            continue
+        
+        if i + jump < n:
+            dp[i + jump][j] += dp[i][j]
+        
+        if j + jump < n:
+            dp[i][j + jump] += dp[i][j]
+
+print(dp[n-1][n-1])
