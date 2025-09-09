40ec97a298ea1ec09d0430c611c854bb0f6d7d3d
이상민 <sm1118sm@gmail.com>
2025-09-09 21:40:21 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,27 @@
+from collections import Counter
+
 def solution(participant, completion):
     answer = ''
-    for i in completion:
-        if i in participant:
-            participant.remove(i)
     
-    for j in participant:
-        answer += j
-
+    cp = Counter(participant)
+    cc = Counter(completion)
     
+    cp.subtract(cc)
+    
+    result = []
+    
+    for i,j in cp.items():
+        if j > 0:
+            result.extend([i] * j)
+            
+    result2 = []
+    
+    for i in participant:
+        if i in result:
+            result2.append(i)
+            result.remove(i)
+            
+    for kk in result2:
+        answer += kk
+        
     return answer
