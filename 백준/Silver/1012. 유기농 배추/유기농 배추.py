1c9510c87e8658622fabf9c99cd18120804a81bc
이상민 <sm1118sm@gmail.com>
2025-10-30 11:18:05 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 32776 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,39 @@
 import sys
-sys.setrecursionlimit(10000)
 input = sys.stdin.readline
+sys.setrecursionlimit(10000)
 
-T = int(input())
+n = int(input())
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
 
-def dfs(x, y):
+def dfs(x,y):
     visited[y][x] = True
-
+    
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < M and 0 <= ny < N:
-            if graph[ny][nx] == 1 and not visited[ny][nx]:
+        if 0 <= nx < m and 0 <= ny < n:
+            if not visited[ny][nx] and graph[ny][nx] == 1:
                 dfs(nx, ny)
 
-for _ in range(T):
-    M, N, K = map(int, input().split())
-    
-    graph = [[0] * M for _ in range(N)]
-    visited = [[False] * M for _ in range(N)]
+for _ in range(n):
+    m, n, k = map(int, input().split())
 
-    for _ in range(K):
+    graph = [[0] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
+
+    for _ in range(k):
         x, y = map(int, input().split())
         graph[y][x] = 1
 
     count = 0
-
-    for y in range(N):
-        for x in range(M):
+    
+    for y in range(n):
+        for x in range(m):
             if graph[y][x] == 1 and not visited[y][x]:
                 dfs(x, y)
                 count += 1
-    
+
     print(count)
