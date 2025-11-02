2fd9e6b61e7e57391ec25edc8038350467b17164
이상민 <sm1118sm@gmail.com>
2025-11-02 13:55:41 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,1 +1,10 @@
-SELECT * FROM DEVELOPERS
+-- 코드를 작성해주세요
+select 
+    id,
+    email,
+    first_name,
+    last_name
+from DEVELOPERS
+where skill_code & (select code from skillcodes where name = 'Python') != 0
+or skill_code & (select code from skillcodes where name = 'C#') != 0
+order by id
