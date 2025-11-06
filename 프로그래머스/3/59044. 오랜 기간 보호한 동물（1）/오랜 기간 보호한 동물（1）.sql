7b9da44e75b88b299ee1393cbba3eb59d2a81224
이상민 <sm1118sm@gmail.com>
2025-11-06 21:39:28 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT
+    a.name,
+    a.datetime
+from animal_ins as a
+left join animal_outs as b on a.animal_id = b.animal_id
+where b.animal_id is null
+order by datetime
+limit 3
