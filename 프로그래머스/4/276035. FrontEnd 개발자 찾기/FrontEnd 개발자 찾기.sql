98ec6b4b447db9455770fa28c45dde9d9cfff198
이상민 <sm1118sm@gmail.com>
2025-12-10 16:25:16 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,12 @@
+-- 코드를 작성해주세요
+SELECT 
+    d.ID,
+    d.EMAIL,
+    d.FIRST_NAME,
+    d.LAST_NAME
+FROM DEVELOPERS d
+JOIN SKILLCODES s
+    ON s.CATEGORY = 'Front End'
+WHERE d.SKILL_CODE & s.CODE > 0
+GROUP BY d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+ORDER BY d.ID;
